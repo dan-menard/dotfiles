83c6050fd6ad969f6e16ce08b453b63935dbbe7e
Dan Menard <dan.menard@shopify.com>
2021-04-28 15:07:46 -0400

    install vimplug, so vim plugins can be installed.

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 # Link relevant dotfiles to their proper locations
 cp ~/dotfiles/.aliases ~/.aliases
 cp ~/dotfiles/.vimrc ~/.vimrc
-cp -R ~/nvim ~/.config
+cp -R ~/dotfiles/nvim ~/.config
 cp ~/dotfiles/.zshrc ~/.zshrc
 
 # Install things those dotfiles use.
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 if ! command -v fzf &> /dev/null; then
   sudo apt-get install -y fzf
